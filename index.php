868e9157a0f2c13a966fa46f5333d531daae6e47
CesarRodriguezMundoNatural <cesar.rodriguez@mundonatural.tv>
2017-10-18 14:40:28 -0400

    Comenzamos de nuevo
    Cree un index con una nav para que agregemos varas cosas y probemos el flujo de trabajo

--- index.php
+++ index.php
@@ -2,23 +2,12 @@
 <html>
 <head>
 	<title>Aplicacion</title>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="css/estilos.css">
-    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
-    <script type="text/javascript" src="js/boostrap.js"></script>
+	<?php include('iphp/header.php'); ?>
 </head>
 <body>
-<?php include('iphp/header.php'); ?>
-<h1>titulo del parrafo</h1>
-
-<span>Hola mundo</span>
-<ul>
-	<li>Hola</li>
-	<li>prueba</li>
-</ul>
+<?php include('iphp/menu.php'); ?>
+<h1>Test de trabajo</h1>
 
 <?php include('iphp/footer.php'); ?>
-<script type="text/javascript" src="js/script.js"></script>
-
 </body>
 </html>
