77505aa957b46df03108d34238dc7838457230b6
yrincon <yoselin.rincon@mundonatural.tv>
2017-10-18 14:58:11 -0400

    Ingrese el footer

---
+++ iphp/footer.php
@@ -0,0 +1,37 @@
+<footer class="page-footer elegant-color center-on-small-only" style="margin-bottom: 0px;">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-lg-2">
+						
+					</div>
+					<div class="col-lg-2 col-sm-2">
+						<h5 class="title">CONTACT</h5>
+						<p><span class="glyphicon glyphicon-map-marker"></span> Miami, US</p>
+						<p><span class="glyphicon glyphicon-phone"></span> 305-282-9987</p>
+						<p><span class="glyphicon glyphicon-envelope"></span> Elaynepedrosa@realtor.com</p>
+					</div>
+					<div class="col-lg-2 col-sm-2">
+					
+					</div>
+					<div class="col-lg-2 col-sm-2">
+						<h5 class="title">IFORMATION</h5>
+						<ul >
+							<li><a href="#signings">Notary Signings Services</a></li>
+							<li><a href="#realestate">Real Estate Services</a></li>
+							<li><a href="#notarypublic">Notary Public Services</a></li>
+							
+						</ul>
+					</div>
+					<div class="col-lg-2 col-sm-2">
+						<h5>SOCIAL MEDIA LINKS</h5>
+						<a target="_blank" href="https://www.facebook.com/Elaynepedrosarealtor/"><img src="img/facebook-logo.png"></a>
+					</div>
+					<div class="col-lg-2 col-sm-2"></div>
+				</div>	
+				<div class="row">
+					<div class="footer-copyright" >
+						<p>Copyright &copy; 2017 </p>
+					</div>
+			</div>
+			</div>
+		</footer>
