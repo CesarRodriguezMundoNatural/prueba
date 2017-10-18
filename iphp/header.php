83ba60191d3ed0c1ce44658c4cebff19fd41e7ba
flaze24 <elleryjose72@gmail.com>
2017-10-18 15:01:06 -0400

    agregar carta de prueba

--- iphp/header.php
+++ iphp/header.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="css/estilos.css">
 <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
-<script type="text/javascript" src="js/boostrap.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
 <script type="text/javascript" src="js/script.js"></script>
